fd4a3ba74aaa7aa7df5813411713940771d681fc
Tom-chack <tom.controlcenter@gmail.com>
2021-04-21 15:10:56 +0300

    Homework 29

--- server.js
+++ server.js
@@ -5,26 +5,21 @@ const fs = require('fs');
 
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-
     db.connect(config.link, {useUnifiedTopology:true, useNewUrlParser:true})
         .then(()=>{
-
             res.set('Content-Type', 'text/html');
             let message = ( req.query.user ) ? req.query.user + ' is updated!' : '';
-           
             User.find({})
             .lean()
             .then( results =>{
-
                 let users_html = fs.readFileSync('./public/users.html', {encoding:'utf-8'});
                 let user_list = '';
-
                 users_html = users_html.replace(/\{message\}/g, message );
-
                 results.forEach( user => {
                     let user_html = fs.readFileSync('./public/user.html', {encoding:'utf-8'});
                     user_html = user_html.replace(/\{name\}/g, user.name);
@@ -33,37 +28,28 @@ app.get('/', function (req, res) {
                     user_html = user_html.replace(/\{id\}/g, user._id);
                     user_list += user_html;
                 } );
-                
                 users_html = users_html.replace('{users}', user_list);
                 res.write(users_html);
                 res.end();
-
             }).catch( e => { throw e });
-
         }).catch( e => { throw e });
 });
 
 
 app.post('/save', function (req, res) {
-
     db.connect(config.link, {useUnifiedTopology:true, useNewUrlParser:true})
         .then(()=>{
-
             res.set('Content-Type', 'text/html');
-           
             let _id = req.body._id;
             let name = req.body.name;
             let email = req.body.email;
             let tel = req.body.tel;
-
             User.findByIdAndUpdate(_id, { name:name, email:email, tel:tel})
                 .then( result=> {
                     res.redirect('/?m=1&user=' + name);
                 })
                 .catch( e => { throw e });
-
         }).catch( e => { throw e });
-
 });
 
 app.listen(80, function () {
