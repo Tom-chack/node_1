ada135a262212d6b08161a5afe3d90918bf3cb1a
Tom-chack <tom.controlcenter@gmail.com>
2021-04-21 09:07:27 +0300

    Connect to db

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const db = require('mongoose');
+const config = require('./config/');
+const User = require('./models/user');
+
 const express = require('express');
 const app = express();
 
 
 app.get('/', function (req, res) {
     res.send('Hello Dev!');
+
+    db.connect(config.link, {useUnifiedTopology:true, useNewUrlParser:true})
+        .then(()=>{
+            console.log('Connected to MongoDB!');
+           
+            
+        }).catch( e => {
+            console.log(e);
+        });
+
 });
 
 app.listen(80, function () {
